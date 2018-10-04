bc55aa4dddf0a6379bbfa02909b107a927f2cbf0
Manny Pissanos <mpissano@gmail.com>
2018-10-04 17:45:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    self.word = word
+  end
+  
+  def match(arr)
+    # binding.pry
+    match_arr = []
+    arr.each { |w| 
+      if w.scan(/\w/).sort == self.word.scan(/\w/).sort 
+        match_arr << w 
+      end
+      }
+    match_arr
+  end
+  
+end
